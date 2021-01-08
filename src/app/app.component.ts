af81385d61f1c2d382761860c7f6586289b909c9
Lea Dalibard <leadalibard@gmail.com>
2021-01-08 13:55:18 +0100

    Binding properties to DOM element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'my awesome app';
+  isAuth = false;
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
 }
+// Bouton s'active après 4 sec, simulation d'authentification
