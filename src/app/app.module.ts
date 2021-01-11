2f51efe82aa619d04f880c03075a9b023600bd1e
Lea Dalibard <leadalibard@gmail.com>
2021-01-11 14:49:42 +0100

    Added Auth-guard services, now page accessible only if logged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
-import { AppareilComponent } from './appareil/appareil.component';
-import { FormsModule } from '@angular/forms';
+import {AppComponent} from './app.component';
+import {MyFirstComponentComponent} from './my-first-component/my-first-component.component';
+import {AppareilComponent} from './appareil/appareil.component';
+import {FormsModule} from '@angular/forms';
 import {AppareilService} from './services/appareil.service';
 import {AuthService} from './services/auth.service';
-import { AuthComponent } from './auth/auth.component';
-import { AppareilViewComponent } from './appareil-view/appareil-view.component';
-import { Routes } from '@angular/router';
+import {AuthGuard} from './services/auth-guard.service';
+import {AuthComponent} from './auth/auth.component';
+import {AppareilViewComponent} from './appareil-view/appareil-view.component';
+import {Routes} from '@angular/router';
 import {RouterModule} from '@angular/router';
-import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
-import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import {SingleAppareilComponent} from './single-appareil/single-appareil.component';
+import {FourOhFourComponent} from './four-oh-four/four-oh-four.component';
 
 const appRoutes: Routes = [
-  { path: 'appareils', component: AppareilViewComponent },
-  { path: 'appareils/:id', component: SingleAppareilComponent },
-  { path: 'auth', component: AuthComponent },
-  { path: '', component: AppareilViewComponent },
-  {path:'not-found', component: FourOhFourComponent},
-  {path:'**',redirectTo:'not-found'}
+  {path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent},
+  {path: 'appareils/:id', component: SingleAppareilComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: '', component: AppareilViewComponent},
+  {path: 'not-found', component: FourOhFourComponent},
+  {path: '**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
@@ -38,7 +39,8 @@ const appRoutes: Routes = [
     FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [AppareilService, AuthService],
+  providers: [AppareilService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
